760f67fd6310ad9f2977b99fef64795e496e993a
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-06-14 16:21:29 +0530

    Add files via upload

---
+++ SignException.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sjcet
+ */
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class SignException
+{
+	public static void main(String args[]) 
+	{
+		double total = 0, N, userInput;
+		Scanner input = new Scanner(System.in);
+		while (true)
+		{
+		 System.out.print("Enter how many numbers(N) to calculate average:");
+		 userInput = input.nextDouble();
+		  if (userInput > 0)
+		  {
+			N = userInput;
+			break;
+		  } 
+		  else
+				System.out.println("N must be positive.");
+		}
+		for (int i = 0; i < N; i++) 
+		{
+			while (true) 
+			{
+				System.out.print("Enter number:");
+				try 
+				{
+					userInput = input.nextDouble();
+					total += userInput;
+					break;
+
+				}
+				catch (InputMismatchException e) 
+				{
+					input.nextLine();
+					System.out.println("Input must bea number. Try again");
+				} 
+			} 
+		}
+		System.out.println("Average: "+ total / N);
+	}
+}
