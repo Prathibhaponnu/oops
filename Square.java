5715d0bee22043e7acaa616fb7572cb294f07a1b
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-07-13 17:51:02 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphics;
+
+/**
+ *
+ * @author Acer
+ */
+public class Square {
+    public void area(double a)
+    {
+        System.out.println("AREA of square:"+(4*a));
+        
+    }
+}
