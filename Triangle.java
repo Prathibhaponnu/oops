8469822c30b4cdc030d1645837a104b5caef1cd0
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-07-13 17:48:19 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphics;
+
+/**
+ *
+ * @author Acer
+ */
+public class Triangle {
+        public void area(double b,double h)
+        {
+           System.out.println("Area of triangle"+((b*h)/2)); 
+        }
+    }
+    
+
+
+
