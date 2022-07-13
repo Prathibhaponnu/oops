68db0d0c437e84845c6780cf79b77b7a49a060db
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-07-13 17:47:00 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphics;
+
+/**
+ *
+ * @author Acer
+ */
+public class Rectangle {
+    public void area(double l,double b)
+    {
+        System.out.println("Area of rectangle:"+(l+b));
+        
+    }
+    
+}
