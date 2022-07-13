f95d46443eecd3e68805ba2ec77321577965373f
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-07-13 17:49:40 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphics;
+
+/**
+ *
+ * @author Acer
+ */
+public class Circle {
+    public void area(double r)
+    {
+        System.out.println("Area of circle:"+3.14*(r*r));
+        
+    }
+    
+}
